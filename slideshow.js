9347589660389eb294f5a5f877a425f1302af0b4
Matthew McGuff <mmcguff@byui.edu>
2020-06-17 10:04:27 -0600

    add copied carousel

--- slideshow.js
+++ slideshow.js
@@ -1,30 +1,39 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0; 
+showSlides(); // call showslide method 
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+function showSlides() 
+{ 
+	var i; 
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+	// get the array of divs' with classname image-sliderfade 
+	var slides = document.getElementsByClassName("image-sliderfade"); 
+	
+	// get the array of divs' with classname dot 
+	var dots = document.getElementsByClassName("dot"); 
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+	for (i = 0; i < slides.length; i++) { 
+		// initially set the display to 
+		// none for every image. 
+		slides[i].style.display = "none"; 
+	} 
+
+	// increase by 1, Global variable 
+	slideIndex++; 
+
+	// check for boundary 
+	if (slideIndex > slides.length) 
+	{ 
+		slideIndex = 1; 
+	} 
+
+	for (i = 0; i < dots.length; i++) { 
+		dots[i].className = dots[i].className. 
+							replace(" active", ""); 
+	} 
+
+	slides[slideIndex - 1].style.display = "block"; 
+	dots[slideIndex - 1].className += " active"; 
+
+	// Change image every 2 seconds 
+	setTimeout(showSlides, 2000); 
+} 
